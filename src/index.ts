31ce72962f5b70d76bca710178836822c0b32970
Nicolas Pope <nicolas.pope@utu.fi>
2022-06-21 05:27:25 +0000

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './Peer';
 export { default as channels } from './channels.schema.json';
 export * from './stream';
+export * from './channel';
+export * from './packets';
+export * from './codecs';
